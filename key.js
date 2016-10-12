1f3ac6ed5dde9011d285741ad4860ba8e7222c8a
Ryan Seery <Ryan Seery>
2016-10-12 17:09:49 -0400

    still trying

--- key.js
+++
@@ -1,9 +0,0 @@
-console.log('this is loaded')
-
-exports.twitterKeys = {
-  consumer_key:'sZl7B3MdRkmiUcWBrzhEKPcTK'
-  consumer_secret: 'TndFbEJqGoagpOBfJ4ERsR9rzz9D3Qcwe85VC5H6bNZIqZNIs8'
-  access_token_key:'384131341-qScwVKUVM1fRAYb7lQMxDPSmHTJFEjXo15bGSBia'
-  access_token_secret: 'E4D5q6FYx3oR8w1awrBiXiESvHDrCUhGjOlzrzqmzktAm'
-};
-
